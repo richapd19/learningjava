d6f6a33e77d84d8f2aa0aef322d069eea1dda906
java program <richapd19@gmail.com>
2025-12-30 09:40:48 +0800

    28/dec/2025 hello world.

---
+++ src/exc_java_basic/javavariables.java
@@ -0,0 +1,15 @@
+package exc_java_basic;
+
+public class javavariables {
+
+    public static void main(String[] args)
+    {
+
+
+            int a = 10;
+            int b = 20;
+
+            System.out.println(a + b);
+
+        }
+    }
