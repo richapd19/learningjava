b76b8d79d68a0a73d32ddafb1518466788f71b2c
java program <richapd19@gmail.com>
2025-12-30 10:24:14 +0800

    28/dec/2025 hello world.

---
+++ src/exc_java_basic/practice_variables.java
@@ -0,0 +1,15 @@
+package exc_java_basic;
+
+public class practice_variables {
+    public static void main(String[]args)
+    {
+        String x= "automation";
+        String y="testing";
+        int a=100;
+        char b='%';
+        boolean c=true;
+        System.out.println(x+y);
+        System.out.println(a + b);
+
+    }
+}
